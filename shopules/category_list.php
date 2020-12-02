b33f09cd37c7786b92a90284b782eacf2761cc8d
yathaw <yathawmyatnoe007@gmail.com>
2020-12-02 11:28:59 +0630

    Category C|R

---
+++ shopules/category_list.php
@@ -0,0 +1,72 @@
+<?php 
+    require 'db_connect.php';
+	require ('backendheader.php');
+
+    $sql = "SELECT * FROM categories ORDER BY id DESC";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+
+    $categories = $stmt->fetchAll();
+
+?>
+
+<div class="app-title">
+    <div>
+        <h1> <i class="icofont-list"></i> Category </h1>
+    </div>
+    <ul class="app-breadcrumb breadcrumb side">
+        <a href="category_new.php" class="btn btn-outline-primary">
+            <i class="icofont-plus"></i>
+        </a>
+    </ul>
+</div>
+<div class="row">
+    <div class="col-md-12">
+        <div class="tile">
+            <div class="tile-body">
+                <div class="table-responsive">
+                    <table class="table table-hover table-bordered" id="sampleTable">
+                        <thead>
+                            <tr>
+                              <th>#</th>
+                              <th>Name</th>
+                              <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            <?php 
+                                $i = 1;
+                                foreach($categories as $category){
+
+                                $cid = $category['id'];
+                                $name = $category['name'];
+                            ?>
+                            <tr>
+                                <td> <?= $i++; ?>. </td>
+                                <td> <?= $name; ?> </td>
+                                <td>
+                                    <a href="" class="btn btn-warning">
+                                        <i class="icofont-ui-settings"></i>
+                                    </a>
+
+                                    <a href="" class="btn btn-outline-danger">
+                                        <i class="icofont-close"></i>
+                                    </a>
+                                </td>
+                            </tr>
+
+                            <?php } ?>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php 
+
+	require ('backendfooter.php');
+?>
